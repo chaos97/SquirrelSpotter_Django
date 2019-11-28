4000f56488c9dae2b6163d57915b20c349361160
chaos97 <572214728@qq.com>
2019-11-28 01:46:53 +0000

    Build up the map path

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from sightings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sightings/',include('sightings.urls')),
+    path('map/',views.squirrel_map),
 ]
