81b589482cde3afe16304e8e1ea91cdb00f56356
Wenyu Han <wh2374@columbia.edu>
2019-12-02 05:16:22 +0000

    add url in project.url
    add url in project/urls.py

--- project/urls.py
+++ project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sightings/',include('sightings.urls')),
+    path('sightings/add/',include('sightings.urls')),
 ]
