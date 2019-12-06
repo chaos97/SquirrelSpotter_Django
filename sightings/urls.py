27208b41c699d38c2de091f8815e9a2621f3e640
chaos97 <572214728@qq.com>
2019-12-06 15:57:27 +0000

    Add a dashboard webpage to /stats path

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
         path('',views.all_squirrels),
         path('add/',views.add_record),
+        path('stats/',views.stats),
         path('<str:squirrel_id>/',views.squirrel_details),
         ]
